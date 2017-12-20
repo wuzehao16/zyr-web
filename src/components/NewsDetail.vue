b4ab2fba71e9bbc189c3dfc1ef3edf4d9716d76d
aisic <wuzehao16@qq.com>
2017-12-20 14:36:33 +0800

    添加新闻详情页面

---
+++ src/components/NewsDetail.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="news-detail">
+    <iframe :src="url" width="1000" height="1000"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: '',
+    }
+  },
+  mounted() {
+    console.log(this.$route.params.url)
+    this.url = 'http://47.104.27.184' + this.$route.params.url
+  },
+}
+</script>
+
+<style lang="scss">
+.news-detail{
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
